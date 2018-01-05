8cafe4aecf1a806148a274b30717601da8c2f0ff
Jimmyord <jimmy.ordonneau@gmail.com>
2018-01-05 01:24:47 +0100

    Gestion des dossiers de sauvegarde

--- files/save.sh
+++ files/save.sh
@@ -1,2 +1,16 @@
 #Set Realmname to enable save compatibility
-ls ./save | 
+nbsvg=$(find /opt/minecraft/save -type d | wc -l)
+if [[ $nbsvg -eq 0 ]]
+then
+	read -p "Thanks to set a name for your new Realm." realmname
+elif [[ $nbsvg -eq 1 ]]
+then
+	realname=$(ls -l ./save/ | awk '{print $1}')
+	echo "Save found in the directory : "$realmname
+else [[ $nbsvg -gt 1 ]]
+	realname=$(ls -l ./save/ | awk '{print $1}')
+	clear
+	echo "Saves found : "$realmname
+	read -p "Thanks to choose the good save : " realmname
+fi
+sed "/30/c level-name=$realmname" server.properties
