ea9cbb2cd69752161659c65dd525185cb6988ab4
Jimmyord <32267874+Jimmyord@users.noreply.github.com>
2018-01-05 01:58:33 +0100

    Update save.sh

--- save.sh
+++ save.sh
@@ -1,16 +1,17 @@
 #Set Realmname to enable save compatibility
-nbsvg=$(find /var/lib/docker/volume/saveminecraft/_data/save/ -type d | wc -l)
+nbsvg=$(find /var/lib/docker/volumes/saveminecraft/_data/save/ -type d | wc -l)
 if [[ $nbsvg -eq 0 ]]
 then
 	read -p "Thanks to set a name for your new Realm." realmname
 elif [[ $nbsvg -eq 1 ]]
 then
-	realmname=$(ls -l /var/lib/docker/volume/saveminecraft/_data/save/ | awk '{print $1}')
+	realmname=$(ls -l /var/lib/docker/volumes/saveminecraft/_data/save/ | awk '{print $1}')
 	echo "Save found in the directory : "$realmname
 else [[ $nbsvg -gt 1 ]]
-	realmname=$(ls -l /var/lib/docker/volume/saveminecraft/_data/save/ | awk '{print $1}')
+	realmname=$(ls -l /var/lib/docker/volumes/saveminecraft/_data/save/ | awk '{print $1}')
 	clear
 	echo "Saves found : "$realmname
 	read -p "Thanks to choose the good save : " realmname
 fi
-sed "/30/c level-name=$realmname" /var/lib/docker/volume/saveminecraft/_data/server.properties
+sed "/30/c level-name=$realmname" /var/lib/docker/volumes/saveminecraft/_data/server.properties
+docker-compose restart minecraft
