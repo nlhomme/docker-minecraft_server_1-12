0778434debf80d2abe06d4862bc0f3306bfcd765
Jimmyord <32267874+Jimmyord@users.noreply.github.com>
2018-02-20 21:25:29 +0100

    Update save.sh
    Amélioration de l'affichage

--- save.sh
+++ save.sh
@@ -3,16 +3,17 @@
 nbsvg=$(find /var/lib/docker/volumes/minecraft_saveminecraft/_data/save/* -maxdepth 0 -type d 2>/dev/null | wc -l)
 if [[ $nbsvg -eq 0 ]]
 then
-        read -pr "Thanks to set a name for your new Realm." realmname
+        read -p "Thanks to set a name for your new Realm." realmname
 elif [[ $nbsvg -eq 1 ]]
 then
         realmname="$(find /var/lib/docker/volumes/minecraft_saveminecraft/_data/save/* -maxdepth 0 -type d)"
         echo "Save found in the directory : $realmname"
-else [[ $nbsvg -gt 1 ]]
+elif [[ $nbsvg -gt 1 ]]
+then
         realmname="$(find /var/lib/docker/volumes/minecraft_saveminecraft/_data/save/* -maxdepth 0 -type d)"
         clear
         echo "Saves found : $realmname"
-        read -pr "Thanks to choose the good save : " realmname
+        read -p "Thanks to choose the good save : " realmname
 fi
 #Recuperation du nom de la sauvegarde
 nbtour=$(echo "$realmname" | grep -o / | wc -l)
@@ -24,7 +25,10 @@ i=$(($i+1))
 done)
 #Ajout des paramètres de la sauvegarde
 sed -i "30c level-name=$realmname" /var/lib/docker/volumes/minecraft_saveminecraft/_data/server.properties
-cd /var/lib/docker/volumes/minecraft_saveminecraft/_data/
-ln -s ./save/"$realmname" "$realmname"
-cd -
+if [[ $nbsvg -ne 0 ]]
+then
+        cd /var/lib/docker/volumes/minecraft_saveminecraft/_data/
+        ln -s ./save/"$realmname" "$realmname"
+        cd -
+fi
 docker-compose restart minecraft
